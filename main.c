565e32d5cb9d9cdc3085e47e82d6124a50ef40fd
Yi Zhang <zhanyi@kean.edu>
2024-11-20 14:17:27 -0800

    change printPath

--- main.c
+++ main.c
@@ -16,14 +16,24 @@ int main() {
 
     // Initialize vehicles
     initializeVehicles(vehicles);
+    Vehicle *v = &vehicles[5];
+    RoutingResult result = aStar(v->current.x, v->current.y, v->destination.x, v->destination.y);
+    printf("Start: (%d, %d)\n", v->current.x, v->current.y);
+    printf("Destination: (%d, %d)\n", v->destination.x, v->destination.y);
+    for (int i = 0; i < result.pathTaken; i++) {
+        printf("(%d, %d) ", result.route.locations[i].x, result.route.locations[i].y);
+    }
+    const int MAX_STEP = 1000;
+    int step = 0;
+    int vehicleCount = NUM_VEHICLES;
+    while (step < MAX_STEP && vehicleCount > 0) {
+        for (int i = 0; i < GRID_WIDTH; i++) {
+            for (int j = 0; j < GRID_HEIGHT; j++) {
+                Node *current = &grid[i][j];
 
-    Vehicle *v = &vehicles[0];
-    int sp = aStar(v->current.x, v->current.y, v->destination.x, v->destination.y);
-    printf("With volume: sp = %d, md = %d\n", sp, heuristic(v->current.x, v->current.y, v->destination.x, v->destination.y));
-    initializeGrid(&map);
-    sp = aStar(v->current.x, v->current.y, v->destination.x, v->destination.y);
-    printf("Without volume: sp = %d, md = %d\n", sp, heuristic(v->current.x, v->current.y, v->destination.x, v->destination.y));
-
-
+            }
+        }
+        step++;
+    }
     return 0;
 }
